63178c1d2471dfe6a300c2a1f2d6001792cbc620
hrahul2605 <hrahul2605@gmail.com>
2019-12-16 06:13:04 +0530

    Update v4

--- waterfall/fpMain.js
+++ waterfall/fpMain.js
@@ -1,7 +1,10 @@
 const row = document.querySelector(".row");
 const beerLoad = document.querySelector("#beerLoading");
+const information = document.querySelector(".information");
+let beer;
 
 let allBeer = "";
+let allInfo = "";
 
 const fecthBeers = async () => {
   // console.log(text)
@@ -25,13 +28,63 @@ const fecthBeers = async () => {
       let picture = `<div id="append" class="img" >
     <h4>${beer[i].name}</h4>
     <h4></h4>
-    <img src=${beer[i].image_url} alt="" id=${beer[i].id} onclick="generic(id) ">
+    <img src=${beer[i].image_url} alt="" id=${beer[i].id} onmouseover="generic(id) ">
   </div> `;
       allBeer += picture;
     }
   }
 
   row.innerHTML = allBeer;
+
+  for (let i = 0; i < beer.length; i++) {
+    let picture = `<div id="append" class="img" >
+    <h4>${beer[i].name}</h4>
+    <h4></h4>
+    <img src=${beer[i].image_url} alt="" id=${beer[i].id} onmouseover="generic(id) ">
+  </div> `;
+    allInfo += picture;
+  }
+  
+  information.innerHTML = `
+  
+          
+        <div class="card text-white" style="max-width: 500px; max-height:200px; margin-left:72%; box-shadow:5px 5px 5px black; background-image: linear-gradient(45deg,brown,red,yellow); position:absolute" >
+          <div class="row no-gutters" >
+            <div class="col-md-12 ">
+              <div class="card-body ">
+                <h5 class="card-title ">${beer[20].name}</h5>
+                <h6 class="card-text">
+                  ${beer[20].description}
+                </h6>
+                <p class="card-text">
+                  <small >${beer[20].tagline}</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card text-white" style="margin-left:1250px ;max-width: 500px; max-height:200px; box-shadow:5px 5px 5px black; background-image: linear-gradient(45deg,brown,red,yellow); z-index:-1; " >
+          <div class="row no-gutters" >
+            <div class="col-md-12 ">
+              <div class="card-body ">
+                <h5 class="card-title ">${beer[21].name}</h5>
+                <h6 class="card-text">
+                  ${beer[21].description}
+                </h6>
+                <p class="card-text">
+                  <small >${beer[21].tagline}</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
+  
+  
+
+`;
   // console.log(allBeer);
 };
 
@@ -39,11 +92,23 @@ const fecthBeers = async () => {
 // var myId;
 
 fecthBeers();
-function generic(val) {
+async function generic(val) {
   console.log(val);
-  
+
+  const url = `https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6`;
+  // console.log(url);
+  const beerData = await fetch(url);
+  const beer = await beerData.json();
+
+  console.log(beer);
+  for (let i = 0; i < 25; i++) {
+    if (beer[i].id == val) {
+      // information.innerHTML = `${beer[i].name}`;
+    }
+  }
+
   // localStorage.setItem('ID',beer.val);
-  document.location.href = "./lastpage.html";
+  // document.location.href = "./lastpage.html";
   // const id = document.querySelector("#val");
   // id.addEventListener("click", () => {
 
